d0e31eff4a60ef976fd2d90ae749c441f334882e
Jonas Bak Phillipson <jbakchr@gmail.com>
2022-03-29 14:44:05 +0200

    Added initial log of string to guess

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const quoteToQuess = "Den pegede mod nord ik";
+const quotes = require("./quotes/quotes.json");
 
-// Print quote with underscores
+// Get quote to guess
+const randomIndex = Math.floor(Math.random() * quotes.length);
+// const quoteToGuess = quotes[randomIndex];
+const quoteToGuess = "Den pegede mod nord ik";
+
+let chosenCharsByUser = [];
+let userQuoteGuess = "";
+
+// First we diplay all the characters of the quote to guess with underscores or blanks
+displayQuoteToGuess();
+
+// Then the user gets to either choose a char or guess the entire quote
+
+function displayQuoteToGuess() {
+  let stringToPrint = "\n";
+
+  for (let index in quoteToGuess) {
+    if (quoteToGuess[index] === " ") {
+      stringToPrint += "  ";
+    } else {
+      stringToPrint += "_ ";
+    }
+  }
+
+  console.log(stringToPrint + "\n");
+}
